52eaa0a5c4f4f131ed194e2af063bd958cc76da0
joajak <joachim.jakob@dnv.com>
2025-11-28 23:48:01 +0100

    feat: create func split_period_in_sessions

--- src/amsterdam_parking/session_calculator.py
+++ src/amsterdam_parking/session_calculator.py
@@ -1,8 +1,19 @@
-class ParkingPeriod:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+from copy import copy
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+
+
+@dataclass
+class Period:
+    start: datetime
+    end: datetime
 
 
 def split_period_in_sessions(parking_period, session_duration, break_duration):
-    pass
+    sessions = []
+    session_period = copy(parking_period)
+    while session_period.start < parking_period.end:
+        session_period.end = session_period.start + timedelta(minutes=session_duration)
+        sessions.append(copy(session_period))
+        session_period.start = session_period.end + timedelta(minutes=break_duration)
+    return sessions
--- tests/unit/test_session_calculator.py
+++ tests/unit/test_session_calculator.py
@@ -1,13 +1,113 @@
-from src.amsterdam_parking.session_calculator import (
-    ParkingPeriod,
-    split_period_in_sessions,
+from datetime import datetime
+
+import pytest
+
+from src.amsterdam_parking.session_calculator import Period, split_period_in_sessions
+
+
+@pytest.mark.parametrize(
+    "parking_period, session_duration, break_duration, sessions_validate",
+    [
+        (
+            Period(start=datetime(2025, 11, 28, 13), end=datetime(2025, 11, 28, 14)),
+            5,
+            10,
+            [
+                Period(datetime(2025, 11, 28, 13), datetime(2025, 11, 28, 13, 5)),
+                Period(datetime(2025, 11, 28, 13, 15), datetime(2025, 11, 28, 13, 20)),
+                Period(datetime(2025, 11, 28, 13, 30), datetime(2025, 11, 28, 13, 35)),
+                Period(datetime(2025, 11, 28, 13, 45), datetime(2025, 11, 28, 13, 50)),
+            ],
+        ),
+        (
+            Period(start=datetime(2025, 11, 28, 13, 5), end=datetime(2025, 11, 28, 14)),
+            5,
+            10,
+            [
+                Period(datetime(2025, 11, 28, 13, 5), datetime(2025, 11, 28, 13, 10)),
+                Period(datetime(2025, 11, 28, 13, 20), datetime(2025, 11, 28, 13, 25)),
+                Period(datetime(2025, 11, 28, 13, 35), datetime(2025, 11, 28, 13, 40)),
+                Period(datetime(2025, 11, 28, 13, 50), datetime(2025, 11, 28, 13, 55)),
+            ],
+        ),
+        (
+            Period(start=datetime(2025, 11, 28, 13), end=datetime(2025, 11, 28, 14, 5)),
+            5,
+            10,
+            [
+                Period(datetime(2025, 11, 28, 13), datetime(2025, 11, 28, 13, 5)),
+                Period(datetime(2025, 11, 28, 13, 15), datetime(2025, 11, 28, 13, 20)),
+                Period(datetime(2025, 11, 28, 13, 30), datetime(2025, 11, 28, 13, 35)),
+                Period(datetime(2025, 11, 28, 13, 45), datetime(2025, 11, 28, 13, 50)),
+                Period(datetime(2025, 11, 28, 14, 0), datetime(2025, 11, 28, 14, 5)),
+            ],
+        ),
+        (
+            Period(
+                start=datetime(2025, 11, 28, 13), end=datetime(2025, 11, 28, 14, 10)
+            ),
+            5,
+            10,
+            [
+                Period(datetime(2025, 11, 28, 13), datetime(2025, 11, 28, 13, 5)),
+                Period(datetime(2025, 11, 28, 13, 15), datetime(2025, 11, 28, 13, 20)),
+                Period(datetime(2025, 11, 28, 13, 30), datetime(2025, 11, 28, 13, 35)),
+                Period(datetime(2025, 11, 28, 13, 45), datetime(2025, 11, 28, 13, 50)),
+                Period(datetime(2025, 11, 28, 14, 0), datetime(2025, 11, 28, 14, 5)),
+            ],
+        ),
+        (
+            Period(start=datetime(2025, 11, 28, 13), end=datetime(2025, 11, 28, 14, 5)),
+            10,
+            10,
+            [
+                Period(datetime(2025, 11, 28, 13), datetime(2025, 11, 28, 13, 10)),
+                Period(datetime(2025, 11, 28, 13, 20), datetime(2025, 11, 28, 13, 30)),
+                Period(datetime(2025, 11, 28, 13, 40), datetime(2025, 11, 28, 13, 50)),
+                Period(datetime(2025, 11, 28, 14, 0), datetime(2025, 11, 28, 14, 5)),
+            ],
+        ),
+    ],
+    ids=[
+        "13:00-14:00@5/10",
+        "13:05-14:00@5/10",
+        "13:00-14:05@5/10",
+        "13:00-14:10@5/10",
+        "13:00-14:05@10/10",
+    ],
 )
-
-
-def test_split_period_in_sessions():
-    parking_period = ParkingPeriod(start="13:00", end="14:00")
-    session_duration = 5
-    break_duration = 10
+def test_split_period_in_sessions(
+    parking_period, session_duration, break_duration, sessions_validate
+):
+    print(parking_period)
     sessions = split_period_in_sessions(
         parking_period, session_duration, break_duration
     )
+    assert sessions
+    assert len(sessions) == len(sessions_validate)
+    for i in range(len(sessions)):
+        assert sessions[i] == sessions_validate[i]
+
+
+# def test_split_period_in_sessions():
+#     parking_period = Period(
+#         start=datetime(2025, 11, 28, 13, 0), end=datetime(2025, 11, 28, 14)
+#     )
+#     session_duration = 5
+#     break_duration = 10
+#
+#     sessions = split_period_in_sessions(
+#         parking_period, session_duration, break_duration
+#     )
+#
+#     assert sessions
+#     assert len(sessions) == 4
+#
+#     sessions_validate = [
+#         Period(datetime(2025, 11, 28, 13), datetime(2025, 11, 28, 13, 5)),
+#         Period(datetime(2025, 11, 28, 13, 15), datetime(2025, 11, 28, 13, 20)),
+#         Period(datetime(2025, 11, 28, 13, 30), datetime(2025, 11, 28, 13, 35)),
+#         Period(datetime(2025, 11, 28, 13, 45), datetime(2025, 11, 28, 13, 50)),
+#     ]
+#     for i in range(len(sessions_validate)):
+#         assert sessions[i] == sessions_validate[i]
