c7818053d56ce170968a7b95b14a1aff59bc75a5
joajak <joachim.jakob@dnv.com>
2025-11-28 20:55:01 +0100

    chore: clean up structure

--- src/amsterdam_parking/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Amsterdam Parking Automation - Modern Python Package."""
-
-__version__ = "2.0.0"
-__author__ = "Amsterdam Parking Bot Contributors"
-
-from amsterdam_parking.models import ParkingSession
-
-__all__ = ["ParkingSession", "__version__"]
--- src/amsterdam_parking/browser/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Browser automation components."""
-
-from amsterdam_parking.browser.driver import BrowserDriver
-from amsterdam_parking.browser.pages import LoginPage, NewSessionPage
-
-__all__ = ["BrowserDriver", "LoginPage", "NewSessionPage"]
